84af597003797d1be41e180e89662fe765f691ee
HarryHayles-MIC <harry.hayles@morrisonsislandcampus.com>
2025-09-29 16:02:39 +0100

    Update HelloWorld.java

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,12 +1,1 @@
-package HelloWorld;
 
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello " + "world");
-		System.out.println("This is a test beep bloop");
-		System.out.println("Hey Look I can do Maths :" + 7*2);
-	}
-
-}
