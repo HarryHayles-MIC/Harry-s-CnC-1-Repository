b962e72d2a360ea045058d1fed1341f9051b1fba
HarryHayles-MIC <harry.hayles@morrisonsislandcampus.com>
2025-09-30 10:59:05 +0100

    Add files via upload

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,1 +1,17 @@
+package HelloWorld;
 
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello " + "world");
+		System.out.println("This is a test beep bloop");
+		System.out.println("Hey Look I can do Maths :" + 7*2);
+		System.out.println("He said \"I will \never do it again.\"");
+		System.out.println("He said \'I will \never do i\t agai\n.\'");
+		System.out.print("This is an example of Print ");
+		System.out.print("and not println");
+		
+	}
+
+}
